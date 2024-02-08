e3745dbbb92c9192fc47b852b1b6f2a43531a49a
Shashank <shashankmende88@gmail.com>
2024-02-08 13:14:31 +0530

    practice otp checkbox

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+
+import {Component} from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends  Component {
+
+  state = {otp:new Array(4).fill('')}
+
+   handleChange = (e,index)=>{
+
+    const {otp} = this.state
+    if (isNaN(e.target.value)) return false
+
+    this.setState({
+      otp: [...otp.map((data,indx)=> index=== indx? e.target.value: data)]
+    })
+
+    if (e.target.value && e.target.nextSibling){
+      e.target.nextSibling.focus()
+    }
+
+  }
+
+  render(){
+    const {otp} = this.state
+    console.log(otp)
+    return (
+      <div className='otp-container'>
+        <h1>creating otp input fields</h1>
+        {otp.map((data,i)=>
+        <input type='text'
+        maxLength={1}
+        value={data}
+        onChange={(e)=>this.handleChange(e,i)}
+        />
+        )}
+
+
+      </div>
+    
+      )
+
+  }
+ 
 }
 
 export default App;
